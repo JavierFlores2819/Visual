f8e093d5327717a49680ab0034f98c1743ddb36e
JavierFlores2819 <javi.2903@outlook.es>
2020-07-23 16:28:31 -0500

    esto fue un cambio

--- Version_Control/src/version_control/Version_Control.java
+++ Version_Control/src/version_control/Version_Control.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package veersion_control;
+package version_control;
 
 /**
  *
  * @author Javier Flores
  */
-public class Veersion_Control {
+public class Version_Control {
 
     /**
      * @param args the command line arguments
