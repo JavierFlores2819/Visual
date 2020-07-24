84312de3dc84f1e6637561b5187a7842bec2f73c
JavierFlores2819 <javi.2903@outlook.es>
2020-07-24 12:32:36 -0500

    esto fue un cambio

--- Version_Control/src/version_control/Version_Control.java
+++ Version_Control/src/version_control/Version_Control.java
@@ -20,10 +20,7 @@ public class Version_Control {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("esto es un primer cambio");
-        JOptionPane.showMessageDialog(null, "CAMBIO");
-        System.out.println("esto es un 2do cambio");
-         JOptionPane.showMessageDialog(null, "CAMBIO   probando ");
-       
+   
     }
 
 }
